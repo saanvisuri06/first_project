5f48effdeb6b6370d2af491729cf4123a31134c5
saanvisuri06 <saanvisuri06@g.ucla.edu>
2024-10-11 16:10:48 -0700

    animations and tilemaps added

--- Assets/PlayerControl.cs
+++ Assets/PlayerControl.cs
@@ -7,17 +7,21 @@ public class PlayerControl : MonoBehaviour
 {
     private Vector2 movementVector;
     private Rigidbody2D rb;
+    private bool isGrounded = true;
+    private bool isDashing = false;
+    private int score = 0;
+    private SpriteRenderer sr;
+
+    [SerializeField] Animator animator;
     [SerializeField] int speed = 0;
     [SerializeField] int jumpForce = 500;
     [SerializeField] int dashForce = 1000; 
     [SerializeField] float dashDuration = 0.1f; 
 
-    private bool isGrounded = true;
-    private bool isDashing = false;
-
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -47,7 +51,12 @@ public class PlayerControl : MonoBehaviour
     void OnMove(InputValue value)
     {
         movementVector = value.Get<Vector2>();
-        Debug.Log(movementVector);
+
+        animator.SetBool("walk_right_bool", !Mathf.Approximately(movementVector.x, 0));
+        //if(!Mathf.Approximately(movementVector.x, 0))
+        {
+            sr.flipX = movementVector.x < 0;
+        }
     }
 
     void OnJump(InputValue value)
@@ -77,6 +86,19 @@ public class PlayerControl : MonoBehaviour
 
         rb.velocity = new Vector2(movementVector.x * speed, rb.velocity.y);
     }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Collectable"))
+        {
+            other.gameObject.SetActive(false);
+            score++;
+            Debug.Log("My score is " + score);
+        }
+    }
+
+    
 }
 
 
